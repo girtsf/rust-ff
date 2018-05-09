49bd864a12594dde9cecd7b7e2a0d20471b6ef7f
Girts Folkmanis <girtsf@users.noreply.github.com>
2018-05-09 15:13:46 +0300

    Initial checkin
    It builds and works.
    
    TODO:
    * detect that output is not tty, disable colors

---
+++ src/main.rs
@@ -0,0 +1,54 @@
+extern crate clap;
+extern crate colored;
+extern crate regex;
+extern crate walkdir;
+
+use clap::{App, Arg};
+use colored::Colorize;
+use regex::{Captures, RegexBuilder};
+
+fn ff(case_sensitive: bool, pattern: Option<&str>) {
+    let re = if let Some(re) = pattern {
+        Some(
+            RegexBuilder::new(re)
+                .case_insensitive(!case_sensitive)
+                .build()
+                .unwrap(),
+        )
+    } else {
+        None
+    };
+    let walker = walkdir::WalkDir::new(".");
+    for entry in walker.into_iter().filter_map(|e| e.ok()) {
+        let name = entry.path().to_string_lossy();
+        let mut pretty = if let Some(ref re) = re {
+            // If regexp is given, skip over stuff that doesn't match.
+            if !re.is_match(&name) {
+                continue;
+            }
+            re.replace_all(&name, |x: &Captures| format!("{}", x[0].red()))
+        } else {
+            name
+        };
+        if entry.file_type().is_dir() {
+            pretty += "/";
+        }
+        println!("{}", pretty);
+    }
+}
+
+fn main() {
+    let matches = App::new("ff")
+        .arg(
+            Arg::with_name("case")
+                .short("s")
+                .long("case-sensitive")
+                .help("Case sensitive matching"),
+        )
+        .arg(Arg::with_name("pattern").index(1))
+        .get_matches();
+
+    let case_sensitive = matches.is_present("case");
+    let pattern = matches.value_of("pattern");
+    ff(case_sensitive, pattern);
+}
